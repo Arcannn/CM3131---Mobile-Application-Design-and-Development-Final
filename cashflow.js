cb1d940d7ec0382c871716f58f0a38721cbf9f07
Tyler <tjstorm7@gmail.com>
2022-04-12 20:20:29 +0100

    cashflow

---
+++ cashflow.js
@@ -0,0 +1,11 @@
+var updateButton = document.getElementById("updateButton");
+var overallBal = document.getElementById("overallBal");
+var overallBalText = document.getElementById("overallBalText");
+
+
+
+updateButton.addEventListener("click", function(){
+    overallBal.color = "success";
+    /* values: Rent 700
+     */
+})
